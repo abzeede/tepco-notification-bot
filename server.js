b300ebed69a86d517387d73121faa2308969af04
bank <>
2019-12-09 09:51:08 +0900

    use port from env

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const lineNotification = require('./workers/lineNotification')
 const tepco = require('./services/tepco')
 
+const PORT = process.env.PORT || 8000
+
 // setup express
 const app = express()
 app.use(bodyParser.urlencoded())
@@ -23,6 +25,6 @@ app.post('/webhook', (req, res) => {
 lineNotification.createLineNotificationWorker('0 8 * * *').start()
 console.log('line notification worker is running')
 
-app.listen(8000, () =>
-  console.log(`Tepco Notification bot listening on port 8000!`)
+app.listen(PORT, () =>
+  console.log(`Tepco Notification bot listening on port ${PORT}!`)
 )
