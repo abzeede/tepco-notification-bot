1484c15c62adf2e765957b8935b5ac1ce17bd952
bank <>
2019-12-09 12:03:54 +0900

    change cron timezone to tokyo

--- workers/lineNotification.js
+++ workers/lineNotification.js
@@ -4,37 +4,44 @@ const tepco = require('../services/tepco')
 const config = require('../config')
 
 exports.createLineNotificationWorker = schedule => {
-  return cron.schedule(schedule, async () => {
-    const current = new Date()
-    const reportMonth = current.getMonth() + 1
-    const reportYear = current.getFullYear()
-    const report = await tepco.getUsageReport(reportYear, reportMonth)
+  return cron.schedule(
+    schedule,
+    async () => {
+      const current = new Date()
+      const reportMonth = current.getMonth() + 1
+      const reportYear = current.getFullYear()
+      const report = await tepco.getUsageReport(reportYear, reportMonth)
 
-    await report.save()
+      await report.save()
 
-    const yesterdayUsage = await tepco.getYesterdayUsage()
-    const totalUsageInMonth = await tepco.getTotalUsageInMonth(
-      reportYear,
-      reportMonth
-    )
+      const yesterdayUsage = await tepco.getYesterdayUsage()
+      const totalUsageInMonth = await tepco.getTotalUsageInMonth(
+        reportYear,
+        reportMonth
+      )
 
-    const client = new line.Client({
-      channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-    })
-    client
-      .pushMessage(process.env.LINE_USER_ID, {
-        type: 'text',
-        text: `Yesterday: ${yesterdayUsage} kwh\nTotal: ${totalUsageInMonth.toFixed(
-          2
-        )} kwh\nPrice(EST): ${(
-          totalUsageInMonth * config.PRICE_PRE_UNIT
-        ).toFixed(2)} Yen`,
+      const client = new line.Client({
+        channelAccessToken: process.env.LINE_ACCESS_TOKEN,
       })
-      .then(() => {
-        console.log('message sent!')
-      })
-      .catch(err => {
-        console.log('push message error: ', err)
-      })
-  })
+      client
+        .pushMessage(process.env.LINE_USER_ID, {
+          type: 'text',
+          text: `Yesterday: ${yesterdayUsage} kwh\nTotal: ${totalUsageInMonth.toFixed(
+            2
+          )} kwh\nPrice(EST): ${(
+            totalUsageInMonth * config.PRICE_PRE_UNIT
+          ).toFixed(2)} Yen`,
+        })
+        .then(() => {
+          console.log('message sent!')
+        })
+        .catch(err => {
+          console.log('push message error: ', err)
+        })
+    },
+    {
+      scheduled: true,
+      timezone: 'Asia/Tokyo',
+    }
+  )
 }
